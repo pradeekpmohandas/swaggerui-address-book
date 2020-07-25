2796ace5301957d0c5cb6bd94eadad7a640344f1
pradeek <pradeek.mohandas@com>
2020-07-25 14:25:47 +0400

    first commit

---
+++ src/test/java/com/pradeek/demo/SwaggeruiAddressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeek.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggeruiAddressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
